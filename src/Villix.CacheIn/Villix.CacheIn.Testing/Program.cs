68b6b37f3b6d44074a8b77cf7bf023c8da2eef45
Dominic Martinez <villixmain@gmail.com>
2020-07-27 17:28:05 -0400

    Added CacheIn testing project

---
+++ src/Villix.CacheIn/Villix.CacheIn.Testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Villix.CacheIn.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
